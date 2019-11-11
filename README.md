1fc8806e50e7a10d0002c495f1fc73c051eef424
Patrik <patrik@ptjwebben.se>
2019-11-11 22:06:28 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Booksonic-Export-Booksonic-Metadata-from-ODM-Files
 This code has not been tested much yet. It "should" not be able to do any harm if something goes wrong but use at your own risk
+
+
+I have only tested this script with Windows powershell but I think it should work fine with Powershell Core as well meaning you can probably run it on Linux.
+
+
+I might write a GUI for this later if there is a demand for it. Once Booksonic bridge is released I plan on having that support odm files natively.
---
+++ odmToBooksonicMeta.ps1
@@ -0,0 +1,48 @@
+﻿Function Convert-ForEachData($data, $strPrepend, $lenRemove, $where = {$_}){
+    $str = ""
+    $data| Where-Object $where| Foreach-Object{
+        $str += "$($_."#text")$strPrepend"
+    }
+    return $str.Substring(0,$str.Length-$lenRemove)
+}
+
+$path = $(Read-Host "Enter path to the folder containing the odm file")
+
+try{
+    Set-Location $path -ErrorAction Stop
+}Catch{
+    Write-Warning "Invalid path"
+    Break
+}
+
+$odm = $(Get-ChildItem .| Where-Object {$_.FullName -like "*.odm"}| Select-Object -first 1).Name
+if($odm -ne "" -AND $odm -ne $null){
+    $loandata = $([xml](Get-Content $odm))
+    $metadata = ([xml]$($loandata.OverDriveMedia."#cdata-section")).Metadata
+
+    
+    <# 
+    
+    # === These are not used in the current implementation of the booksonic server === #
+    
+    $title = $metadata.SortTitle
+    $publisher = $metadata.Publisher
+    $author = Convert-ForEachData -data $metadata.Creators.Creator -strPrepend " & " -lenRemove 3 -where { $_.role -eq "Author" }
+    $genres = Convert-ForEachData -data $metadata.subjects.subject -strPrepend "," -lenRemove 1
+    $language = Convert-ForEachData -data $metadata.Languages.Language -strPrepend "," -lenRemove 1
+    $returnBook = $loandata.OverDriveMedia.EarlyReturnURL
+
+    #>
+    
+    $description = $metadata.Description
+    $narrator = Convert-ForEachData -data $metadata.Creators.Creator -strPrepend " & " -lenRemove 3 -where { $_.role -eq "Narrator" }
+    $coverURL = $metadata.CoverUrl
+
+    $description| Out-File "desc.txt"
+    $narrator| Out-File "reader.txt"
+    Invoke-Webrequest $coverURL -outFile "cover.jpg"
+
+    Write-Host "Description, narrator (reader) and cover was exported"
+}else{
+    Write-Warning "The path contains no odm file"
+}
